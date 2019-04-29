549079bfeccd93b5d1225a7e6b630470c0f65a5a
Pat McDonald <patmcdonald@me.com>
2019-04-29 12:21:53 +0100

    Code and source

---
+++ bogosort.py
@@ -0,0 +1,22 @@
+# https://www.geeksforgeeks.org/python-program-for-bogosort-or-permutation-sort/
+
+# Sorts array a[0..n-1] using Bogo sort 
+def bogoSort(a): 
+    n = len(a) 
+    while (is_sorted(a)== False): 
+        shuffle(a) 
+  
+# To check if array is sorted or not 
+def is_sorted(a): 
+    n = len(a) 
+    for i in range(0, n-1): 
+        if (a[i] > a[i+1] ): 
+            return False
+    return True
+  
+# To generate permuatation of the array 
+def shuffle(a): 
+    n = len(a) 
+    for i in range (0,n): 
+        r = random.randint(0,n-1) 
+        a[i], a[r] = a[r], a[i] 
