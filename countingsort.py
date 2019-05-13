9e0e62ed4976a2eb2d1024f4730e774b5bc85aff
Pat McDonald <patmcdonald@me.com>
2019-05-13 12:26:13 +0100

    Countingsort code upload

--- countingsort.py
+++ countingsort.py
@@ -1,28 +1,20 @@
 # Code source: https://github.com/TheAlgorithms/Python/blob/master/sorts/radix_sort.py
 
-def radixsort(lst):
-    RADIX = 10
-    placement = 1
+# Code source: 
+# https://www.w3resource.com/python-exercises/data-structures-and-algorithms/python-search-and-sorting-exercise-10.php
 
-    # get the maximum number
-    max_digit = max(lst)
+def counting_sort(array1, max_val):
+    m = max_val + 1
+    count = [0] * m                
+    
+    for a in array1:
+    # count occurences
+        count[a] += 1             
+    i = 0
+    for a in range(m):            
+        for c in range(count[a]):  
+            array1[i] = a
+            i += 1
+    return array1
 
-    while placement < max_digit:
-      # declare and initialize buckets
-      buckets = [list() for _ in range( RADIX )]
-
-      # split lst between lists
-      for i in lst:
-        tmp = int((i / placement) % RADIX)
-        buckets[tmp].append(i)
-
-      # empty lists into lst array
-      a = 0
-      for b in range( RADIX ):
-        buck = buckets[b]
-        for i in buck:
-          lst[a] = i
-          a += 1
-
-      # move to next
-      placement *= RADIX
+print(counting_sort( [1, 2, 7, 3, 2, 1, 4, 2, 3, 2, 1], 7 ))
